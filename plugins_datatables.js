93179d641a9f8822e0da3297b0b21a588de0d902
unknown <LA_U265537@DTCEE4557EECC2C.ent.wfb.bank.corp>
2016-04-12 13:48:31 -0700

    Adding a DataTables 1.10 plugin to support dom-select sorting in columns

--- plugins_datatables.js
+++ plugins_datatables.js
@@ -195,3 +195,20 @@ jQuery.fn.dataTableExt.oSort['neg-currency-desc'] = function (a, b) {
 
     return y - x;
 }; // This is just a sample script. Paste your real code (javascript or HTML) here.
+
+/**
+ * Read information from a column of select (drop down) menus and return an
+ * array to use as a basis for sorting.
+ *
+ *  @summary Sort based on the value of the `dt-tag select` options in a column
+ *  @name Select menu data source
+ *  @requires DataTables 1.10+
+ *  @author [Allan Jardine](http://sprymedia.co.uk)
+ */
+$.fn.dataTable.ext.order['dom-select'] = function (settings, col) {
+    return this.api().column(col, {
+        order: 'index'
+    }).nodes().map(function (td, i) {
+        return $('select', td).val();
+    });
+};
